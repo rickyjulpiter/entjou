8de73595250687e9b705c2bb3d693b11030aa4c7
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-01-21 02:02:33 +0700

    try to make form on modal

--- application/views/frontend/view_home.php
+++ application/views/frontend/view_home.php
@@ -63,38 +63,38 @@ $slider1 = $slider2 = $slider;
                 <div class="modal-body">
                     <form class="form-horizontal" action="/action_page.php">
                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="email">Nama:</label>
-                            <div class="col-md-10">
+                            <label class="control-label col-md-4" for="email">Nama:</label>
+                            <div class="col-md-8">
                                 <input type="text" class="form-control" id="email" placeholder="Enter email" name="nama">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="email">No. WA:</label>
-                            <div class="col-md-10">
+                            <label class="control-label col-md-4" for="email">No. WA:</label>
+                            <div class="col-md-8">
                                 <input type="text" class="form-control" id="email" placeholder="Enter email" name="nama">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="email">Email:</label>
-                            <div class="col-md-10">
+                            <label class="control-label col-md-4" for="email">Email:</label>
+                            <div class="col-md-8">
                                 <input type="email" class="form-control" id="email" placeholder="Enter email" name="nama">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="email">Nama Usaha:</label>
-                            <div class="col-md-10">
+                            <label class="control-label col-md-4" for="email">Nama Usaha:</label>
+                            <div class="col-md-8">
                                 <input type="text" class="form-control" id="email" placeholder="Enter email" name="nama">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="control-label col-sm-2" for="email">Deskripsi Singkat Usaha:</label>
-                            <div class="col-md-10">
+                            <label class="control-label col-md-4" for="email">Deskripsi Singkat Usaha:</label>
+                            <div class="col-md-8">
                                 <input type="text" class="form-control" id="email" placeholder="Enter email" name="nama">
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-md-10">
-                                <textarea type="text" class="form-control" id="email" placeholder="Enter email" name="nama"></textarea>
+                            <div class="col-md-8">
+
                             </div>
                         </div>
                         <div class="form-group">
