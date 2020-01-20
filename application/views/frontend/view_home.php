fc52137a6fc6def2826d0968d20f7ca04f2984e3
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-01-21 01:56:50 +0700

    try to make form on modal

--- application/views/frontend/view_home.php
+++ application/views/frontend/view_home.php
@@ -54,7 +54,7 @@ $slider1 = $slider2 = $slider;
 <div class="container">
     <!-- Modal -->
     <div class="modal fade" id="myModal" role="dialog">
-        <div class="modal-dialog modal-sm">
+        <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -64,25 +64,25 @@ $slider1 = $slider2 = $slider;
                     <form class="form-horizontal" action="/action_page.php">
                         <div class="form-group">
                             <label class="control-label col-sm-2" for="email">Email:</label>
-                            <div class="col-sm-10">
+                            <div class="col-md-10">
                                 <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label col-sm-2" for="pwd">Password:</label>
-                            <div class="col-sm-10">
+                            <div class="col-md-10">
                                 <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-sm-offset-2 col-sm-10">
+                            <div class="col-md-offset-2 col-md-10">
                                 <div class="checkbox">
                                     <label><input type="checkbox" name="remember"> Remember me</label>
                                 </div>
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-sm-offset-2 col-sm-10">
+                            <div class="col-md-offset-2 col-md-10">
                                 <button type="submit" class="btn btn-default">Submit</button>
                             </div>
                         </div>
