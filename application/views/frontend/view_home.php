40e5d60eb76e7c9592220e2616aa1647bea77d1a
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-01-21 01:55:16 +0700

    try to make form on modal

--- application/views/frontend/view_home.php
+++ application/views/frontend/view_home.php
@@ -48,14 +48,10 @@ $slider1 = $slider2 = $slider;
     <h2 class="title-default-left" style="text-align: center">Kamu Enterpreneur UMKM ?</h2>
     <h2 class="title-default-left" style="text-align: center">Punya masalah bisnis yang belum terpecahkan ?</h2>
     <h2 class="title-default-left" style="text-align: center">Mari Curhat Bisnis Gratis Bersama Kami.</h2>
-    <a href="<?= base_url('curhat') ?>" class="btn btn-lg btn-danger btn-block">Mulai Curhat Bisnis</a>
+    <button type="button" class="btn btn-lg btn-danger btn-block" data-toggle="modal" data-target="#myModal">Mulai Curhat Bisnis</button>
 </div>
 
 <div class="container">
-    <h2>Small Modal</h2>
-    <!-- Trigger the modal with a button -->
-    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Small Modal</button>
-
     <!-- Modal -->
     <div class="modal fade" id="myModal" role="dialog">
         <div class="modal-dialog modal-sm">
@@ -65,7 +61,32 @@ $slider1 = $slider2 = $slider;
                     <h4 class="modal-title">Modal Header</h4>
                 </div>
                 <div class="modal-body">
-                    <p>This is a small modal.</p>
+                    <form class="form-horizontal" action="/action_page.php">
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="email">Email:</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="pwd">Password:</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <div class="checkbox">
+                                    <label><input type="checkbox" name="remember"> Remember me</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-default">Submit</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
